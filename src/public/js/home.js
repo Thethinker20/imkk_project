5ed9922880bc12ab8d8671961cec87f5e6d26ba7
Thethinker20 <43387401+Thethinker20@users.noreply.github.com>
2022-09-10 21:53:14 -0400

    home_update_mobile

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,23 +1,9 @@
-// $(document).bind("contextmenu",function(e) {
-//     e.preventDefault();
-//    });
+$(document).bind("contextmenu",function(e) {
+    e.preventDefault();
+   });
    
-//    $(document).keydown(function(e){
-//        if(e.which === 123){
-//           return false;
-//        }
-//    });
-
-// function removeKeys(x) {
-//     if (x.matches) { // If media query matches
-//         document.querySelectorAll('#piano_keys > span(-n+10)').forEach(n => {
-//             n.parentNode.removeChild(n)
-//           })
-//     } else {
-//       document.getElementById("divIWantedToHide").style.visibility = "visible";
-//     }
-//   }
-  
-//   var x = window.matchMedia("(min-width: 390px)")
-//   removeKeys(x) // Call listener function at run time
-//   x.addListener(removeKeys)
+   $(document).keydown(function(e){
+       if(e.which === 123){
+          return false;
+       }
+   });
